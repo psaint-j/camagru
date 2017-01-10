2e388f7700d219aa74c48569a965441b88e91fd4
Philippe SAINT-JACQUES <psaint-j@e1r7p20.42.fr>
2017-01-10 15:07:59 +0100

    views home done

---
+++ views/view-index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link href="https://fonts.googleapis.com/css?family=Bungee+Shade|Montserrat|Pacifico|Roboto" rel="stylesheet">
+	<title>Camagru - Hello</title>
+</head>
+<body>
+<h1>Camagru</h1>
+<div class="box">
+<table>
+<tr>
+	<td><button>Login</button></td>
+</tr>
+<tr>
+<td><br></td>
+</tr>
+<tr>
+	<td><input type="text" placeholder="name"></td>
+</tr>
+<tr>
+	<td><input type="text" placeholder="email"></td>
+</tr>
+<tr>
+	<td><input type="password" placeholder="password"></td>
+</tr>
+<tr>
+	<td><input type="password" placeholder="re-enter password"></td>
+</tr>
+<tr>
+	<td><button>Sign Up</button></td>
+</tr>
+</table>
+</div>
+</body>
+</html>
---
+++ views/view-login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<title></title>
+</head>
+<body>
+<h1>login</h1>
+</body>
+</html>

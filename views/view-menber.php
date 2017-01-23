1c46ac73ace7232ced8dd7f0307f1612f048fc56
Philippe SAINT-JACQUES <psaint-j@e2r4p10.42.fr>
2017-01-23 11:20:08 +0100

    webcan work fine need to save image

--- views/view-menber.php
+++ views/view-menber.php
@@ -3,25 +3,24 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="css/menber.css">
 	<link href="https://fonts.googleapis.com/css?family=Bungee+Shade|Montserrat|Pacifico|Roboto" rel="stylesheet">
-	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 	<title>Camagru - menber</title>
 </head>
 <body class="news">
 	<header>
 		<div class="nav">
 			<ul>
-				<li class="home"><a href="#"><img src="svg/safari.svg" /></a></li>
-				<li class="about"><a href="#"><img src="svg/photos.svg" /></a></li>
-				<li class="contact"><a href="logout.php"><img src="svg/logout.svg" /></a></li>
+				<li class="home"><a href="#"><img src="svg/home.svg" alt="home"/></a></li>
+				<li class="photo"><a href="#"><img src="svg/image.svg" alt="take a picture"/></a></li>
+				<li class="logout"><a href="logout.php"><img src="svg/logout.svg" alt="logout"/></a></li>
 			</ul>
 		</div>
 	</header>
 	<div class="box">
-		<video id="video" width="640" height="480" autoplay></video>
+		<video id="video" ></video>
 		<button id="startbutton">Prendre une photo</button>
-		<canvas id="canvas" width="640" height="480"></canvas>
-		<img src="http://placekitten.com/g/320/261" id="photo" alt="photo">
+		<canvas id="canvas"></canvas>
 	</div>
+
 	<script src="js/camera.js" type="text/javascript"></script>
 </body>
 </html>
